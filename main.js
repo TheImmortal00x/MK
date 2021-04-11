9d5a1ecc6005e8689b9edc0fe503f1efcbd229aa
Igor Draganov <eternal.dragon1288@gmail.com>
2021-04-11 12:51:34 +0300

    homework2
    неправильно создал ветку, пришлось переписывать код по-новой

--- main.js
+++ main.js
@@ -1,53 +1,85 @@
+const $arenas = document.querySelector(".arenas")
+const $randomButton = document.querySelector(".button")
+
 const player1 = {
+    player: 1,
     name: "Scorpio",
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['Kunai on a chain'],
-    attack(){
+    attack() {
         console.log(this.name + "Fight...")
     }
 }
 
 const player2 = {
+    player: 2,
     name: "SubZero",
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['Ice scepter'],
-    attack(){
+    attack() {
         console.log(this.name + "Fight...")
     }
 }
 
-function createPlayer(player, obj){
-    const $player1 = document.createElement('div');
-    $player1.classList.add(player)
+function createElement(tag, className) {
+    const $tag = document.createElement(tag)
 
-    const $progressbar = document.createElement('div');
-    $progressbar.classList.add("progressbar")
-    $player1.appendChild($progressbar)
+    if (className) {
+        $tag.classList.add(className)
+    }
 
-    const $character = document.createElement('div');
-    $character.classList.add("character")
-    $player1.appendChild($character)
+    return $tag
+}
 
-    const $life = document.createElement('div');
-    $life.classList.add("life")
+function changeHP(player) {
+    const $playerLife = document.querySelector('.player' + player.player + ' ' + '.life');
+    player.hp -= Math.ceil(Math.random() * 20)
+    if (player.hp <= 0) {
+        player.hp = 0
+    }
+    console.log(player1.name + " is on " + player1.hp + " hp")
+    console.log(player2.name + " is on " + player2.hp + " hp")
+    $playerLife.style.width = player.hp + "%"
+}
+
+$randomButton.addEventListener('click', function () {
+    changeHP(player1)
+    changeHP(player2)
+
+    if (player1.hp <= 0) {
+        console.log(player2.name + " won")
+        console.log(player1.name + " lost")
+    } else if (player2.hp <= 0) {
+        console.log(player1.name + " won")
+        console.log(player2.name + " lost")
+    }
+})
+
+function createPlayer(obj) {
+    const $player = createElement('div', 'player' + obj.player)
+    const $progressbar = createElement('div', 'progressbar')
+    const $character = createElement('div', 'character')
+    const $life = createElement('div', 'life')
+    const $name = createElement('div', 'name')
+    const $img = createElement('img')
+
+    $player.appendChild($progressbar)
+    $player.appendChild($character)
+
     $life.style.width = "100%"
     $life.style.width = obj.hp + "%"
     $progressbar.appendChild($life)
 
-    const $name = document.createElement('div');
-    $name.classList.add("name")
     $name.innerHTML = obj.name
     $progressbar.appendChild($name)
 
-    const $img = document.createElement('img');
     $img.src = obj.img
     $character.appendChild($img)
 
-    const $arenas = document.querySelector(".arenas")
-    $arenas.appendChild($player1)
+    return $player
 }
 
-createPlayer("player1", player1)
-createPlayer("player2", player2)
+$arenas.appendChild(createPlayer(player1))
+$arenas.appendChild(createPlayer(player2))
